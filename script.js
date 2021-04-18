3f2fb70486420bd74775400ac9091ecb5cb8549c
Elronia <zar.catherine@gmail.com>
2021-04-17 23:22:54 -0400

    adding Event Listeners for speech result and end of speech

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ recognition.interimResults = true;
 let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+
+    console.log(transcript);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
